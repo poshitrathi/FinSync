0a1457c642e478234a389179ac548eca49923647
poshitrathi <119343061+poshitrathi@users.noreply.github.com>
2025-07-29 20:20:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['randomuser.me'],
+  images: {
+    domains: [
+      'randomuser.me',
+      'https://zdswivpqybrgbbnziwmu.supabase.co', // replace with your real Supabase domain
+      'cdn.jsdelivr.net',              // example: if using jsDelivr
+      'vercel.app',                    // if loading images from deployed assets
+      'avatars.githubusercontent.com'  // if loading from GitHub
+    ],
   },
 };
 
